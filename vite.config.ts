dcf7436dc8a3a55e901bd7c627c8b7d5421fdc72
MHenriquez <76463428+MHenriquezD@users.noreply.github.com>
2026-08-14 15:36:12 -0600

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: 'portfolio-v2',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))

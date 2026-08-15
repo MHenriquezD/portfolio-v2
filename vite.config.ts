3930d5daaf0034021e59d6ade285e59491bf2439
MHenriquez <76463428+MHenriquezD@users.noreply.github.com>
2026-08-14 22:54:15 -0600

    Update base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base: 'portfolio-v2',
+  base: '/portfolio-v2/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
